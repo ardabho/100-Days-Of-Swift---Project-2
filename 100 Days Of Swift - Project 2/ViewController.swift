e4bc83f5eb84776173e9c1fe14ee447fdf0e3166
Arda Büyükhatipoğlu <ardabho@gmail.com>
2022-05-30 17:40:51 +0300

    Added flag asserts. Designed the user interface.

--- 100 Days Of Swift - Project 2/ViewController.swift
+++ 100 Days Of Swift - Project 2/ViewController.swift
@@ -8,7 +8,14 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var button1: UIButton!
+    @IBOutlet weak var button2: UIButton!
+    @IBOutlet weak var button3: UIButton!
+    
+    var countries = [String]()
+    var score: Int
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
